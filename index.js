da5106aaf39257165428984737d6da5a9ad40168
dumiduhimsara <dumiduhimsara19@gmail.com>
2026-03-15 13:44:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use('/invoices', express.static(path.join(__dirname, 'invoices')));
 app.use('/qr-images', express.static(path.join(__dirname, 'qr-images')));
 
 // --- ඊළඟට ඔයාගේ MongoDB Connection එක ---
-mongoose.connect('mongodb://127.0.0.1:27017/epr_portal')
+const mongoURI = process.env.MONGODB_URI || 'mongodb+srv://dumidu:suha123@cluster0.zkbmh7n.mongodb.net/epr_portal';
   .then(() => console.log("✅ Database Connected Successfully!"))
   .catch(err => console.log("❌ DB Connection Error:", err)); 
 // --- EMAIL CONFIGURATION (මෙන්න මේකයි Transporter එක) ---
