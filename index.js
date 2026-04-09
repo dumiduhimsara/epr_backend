f34e04e7d836abef56b016ed13bbc9ac6d750415
Dumidu Himsara <dumiduhimsara19@gmail.com>
2026-04-09 23:31:00 +0530

    Updated axios with credentials for secure cookies

--- index.js
+++ index.js
@@ -130,8 +130,8 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/auth', authRoutes);         
-app.post('/api/customers/register', customerRoutes); 
-app.use('/api/customers', authenticateToken, customerRoutes);
+//app.post('/api/customers/register', customerRoutes); 
+app.use('/api/customers', customerRoutes);
 app.use('/api/orders', authenticateToken, orderRoutes);     
 app.use('/api/qr', qrRoutes);             
 app.use('/api/partners', partnerRoutes);   
